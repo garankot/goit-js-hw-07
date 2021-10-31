675d81b2adb7dc4fc1ec01642543330b4b010d5b
Taras Garan'ko <garanko.t@gmail.com>
2021-10-31 21:01:50 +0200

    7_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const createItem = galleryItems
+  .map(
+    item => `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+             </a>
+        </div>`
+  )
+  .join('');
 
+gallery.insertAdjacentHTML('beforeend', createItem);
+
+gallery.addEventListener('click', onSelectImage);
+
+function onSelectImage(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onClose: () => {
+            document.removeEventListener('keydown', onEscClisk);
+        },
+    }
+  );
+  instance.show();
+
+  document.addEventListener('keydown', onEscClisk);
+  function onEscClisk(event) {
+    if (event.keyCode === 27) {
+      instance.close();
+    }
+  }
+}
 console.log(galleryItems);
